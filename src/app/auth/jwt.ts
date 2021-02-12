8012379521eff04bc9a8776d4576e787ff26f968
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-02-12 13:57:19 -0300

    chore: remover arquivos desnecessários

--- src/app/auth/jwt.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-import { Artist } from '../entity/artist.entity';
-
-export function sign(user: Artist): string {
-  const secret = process.env.APP_SECRET || 'xHBpZ5Kbac';
-
-  return jwt.sign({ uuid: user.uuid }, secret, { expiresIn: '1d' });
-}
