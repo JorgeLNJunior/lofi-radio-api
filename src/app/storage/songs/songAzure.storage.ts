de23f835f49104b5aa5dcf6ea573c55c3b943533
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-02-16 11:31:46 -0300

    feat: implemetar upload no azure para músicas

--- src/app/storage/songs/songAzure.storage.ts
+++ src/app/storage/songs/songAzure.storage.ts
@@ -1,10 +1,43 @@
+import { BlobServiceClient } from '@azure/storage-blob';
 import { SongBaseStorage } from 'src/@types/storage';
 
 export class SongAzureStorage implements SongBaseStorage {
-  storeSong(song: Express.Multer.File): Promise<string> {
-    throw new Error('Method not implemented.');
+  private containerName = process.env.AZURE_CONTAINER;
+  private account = process.env.AZURE_ACCOUNT;
+  private sas = process.env.AZURE_SAS;
+  private client = new BlobServiceClient(
+    `https://${this.account}.blob.core.windows.net${this.sas}`,
+  );
+
+  async storeSong(song: Express.Multer.File): Promise<string> {
+    const extension = song.originalname.split('.').pop();
+    const name = `${Date.now()}.${extension}`;
+
+    const containerClient = this.client.getContainerClient(
+      this.containerName as string,
+    );
+    const blobClient = containerClient.getBlockBlobClient(
+      `songs/audio/${name}`,
+    );
+
+    await blobClient.uploadData(song.buffer);
+
+    return `https://${this.account}.blob.core.windows.net/${this.containerName}/songs/audio/${name}`;
   }
-  storeCover(cover: Express.Multer.File): Promise<string> {
-    throw new Error('Method not implemented.');
+
+  async storeCover(cover: Express.Multer.File): Promise<string> {
+    const extension = cover.originalname.split('.').pop();
+    const name = `${Date.now()}.${extension}`;
+
+    const containerClient = this.client.getContainerClient(
+      this.containerName as string,
+    );
+    const blobClient = containerClient.getBlockBlobClient(
+      `songs/covers/${name}`,
+    );
+
+    await blobClient.uploadData(cover.buffer);
+
+    return `https://${this.account}.blob.core.windows.net/${this.containerName}/songs/covers/${name}`;
   }
 }
