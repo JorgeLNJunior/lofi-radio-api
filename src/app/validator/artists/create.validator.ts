f4a84518fffa3830bae589b9af077e915de08b1b
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-04-24 18:38:29 -0300

    refactor: separar os regex

--- src/app/validator/artists/create.validator.ts
+++ src/app/validator/artists/create.validator.ts
@@ -2,31 +2,23 @@ import Joi from 'joi';
 
 import { ArtistBody } from '../../../@types/body';
 import { BaseValidator } from '../../../@types/validator';
+import { soundcloudRegex, spotifyRegex, youtubeRegex } from './regex';
 
 export class CreateArtistValidator implements BaseValidator {
   validate(body: ArtistBody): Joi.ValidationResult {
     const schema = Joi.object({
       name: Joi.string().required(),
-      spotifyUrl: Joi.string()
-        .optional()
-        .regex(/^https?:\/\/(www.)?open.spotify\.com\/artist\//)
-        .messages({
-          'string.pattern.base': '"spotifyUrl" must be a valid spotify url',
-        }),
-      youtubeUrl: Joi.string()
-        .optional()
-        .regex(/^https?:\/\/(www.)?youtube\.com\/channel\//)
-        .messages({
-          'string.pattern.base':
-            '"youtubeUrl" must be a valid youtube channel url',
-        }),
-      soundcloudUrl: Joi.string()
-        .optional()
-        .regex(/^https?:\/\/(www.)?soundcloud\.com\//)
-        .messages({
-          'string.pattern.base':
-            '"soundcloudUrl" must be a valid soundcloudUrl url',
-        }),
+      spotifyUrl: Joi.string().optional().regex(spotifyRegex).messages({
+        'string.pattern.base': '"spotifyUrl" must be a valid spotify url',
+      }),
+      youtubeUrl: Joi.string().optional().regex(youtubeRegex).messages({
+        'string.pattern.base':
+          '"youtubeUrl" must be a valid youtube channel url',
+      }),
+      soundcloudUrl: Joi.string().optional().regex(soundcloudRegex).messages({
+        'string.pattern.base':
+          '"soundcloudUrl" must be a valid soundcloudUrl url',
+      }),
     });
 
     return schema.validate(body, { stripUnknown: true });
